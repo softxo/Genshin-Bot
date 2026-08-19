450cf72400041975791023a8de93b52520d174ed
silently <silentvqice@gmail.com>
2026-08-19 15:48:40 +0100

    Update

---
+++ website/static/js/theme.js
@@ -0,0 +1,10 @@
+const savedTheme = localStorage.getItem("theme") || "blue";
+
+document.documentElement.dataset.theme = savedTheme;
+
+
+function setTheme(theme) {
+    document.documentElement.dataset.theme = theme;
+
+    localStorage.setItem("theme", theme);
+}
