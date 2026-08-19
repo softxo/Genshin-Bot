67bb7e38701d398ce97bffd3964846a825a43d24
silently <silentvqice@gmail.com>
2026-08-19 16:00:49 +0100

    Update

--- website/static/js/theme.js
+++ website/static/js/theme.js
@@ -2,9 +2,7 @@ const savedTheme = localStorage.getItem("theme") || "blue";
 
 document.documentElement.dataset.theme = savedTheme;
 
-
 function setTheme(theme) {
     document.documentElement.dataset.theme = theme;
-
     localStorage.setItem("theme", theme);
 }
